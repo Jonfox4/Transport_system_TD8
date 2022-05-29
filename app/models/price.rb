b6f67257c7c188ffc3cc071b9d60f65878961725
Jonathan Ramiro <jonathan_jrs@hotmail.com>
2022-05-29 13:47:56 -0300

    Erros em testes do commit anterior solucionados

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,3 +1,5 @@
 class Price < ApplicationRecord
   belongs_to :shipping_company
+  validates :minimum_weight, :maximum_weight, :maximum_cubic_meter, :minimum_cubic_meter, :price_km, presence: true
+  validates :minimum_weight, :maximum_weight, :maximum_cubic_meter, :minimum_cubic_meter, :price_km, numericality: { only_integer: false }
 end
--- spec/system/prices/company_config_prices_spec.rb
+++ spec/system/prices/company_config_prices_spec.rb
@@ -19,21 +19,20 @@ describe 'Usuário cadastra seus preços' do
     company = ShippingCompany.create!(brand_name: 'One Transportes', corporate_name: 'Empresa One', email_domain: 'one.com', registration_number: 11_111_111_111_111,
                                       address: 'Avenida numero one', city: 'Cidade 1', state: '1')
     user = User.create!(email: 'admin@one.com', password: '123456', role: 'company', shipping_company: company)
-
     # Act
     login_as user
     visit shipping_companies_path
     click_on 'One Transportes'
     click_on 'Configurar Preços'
     click_on 'Cadastrar novo preço'
-    fill_in 'Peso mínimo', with: '0'
+    fill_in 'Peso mínimo', with: '1'
     fill_in 'Peso máximo', with: '10'
-    fill_in 'Minímo m³', with: '0.0001'
-    fill_in 'Máximo m³', with: '0.500'
+    fill_in 'Mínimo m³', with: '1'
+    fill_in 'Máximo m³', with: '5'
     fill_in 'Valor por Km', with: '0.75'
     click_on 'Enviar'
 
-    expect(page).to have_content 'Entre 0 a 0 m³ e peso entre 0.0 e 10.0 kgs o valor é de 0 reais por km.'
+    expect(page).to have_content 'Entre 1.0 e 10.0 Kg e 5.0 a 1.0 m³ o preço por KM é R$ 0.75'
   end
 
   it 'com dados incompletos' do
@@ -44,20 +43,21 @@ describe 'Usuário cadastra seus preços' do
     # Act
     login_as user
     visit shipping_companies_path
-    click_on 'Configurar preços'
-    click_on 'Cadastrar preço'
+    click_on 'One Transportes'
+    click_on 'Configurar Preços'
+    click_on 'Cadastrar novo preço'
     fill_in 'Peso mínimo', with: ''
-    fill_in 'Peso maximo', with: ''
-    fill_in 'Minímo m³', with: ''
+    fill_in 'Peso máximo', with: ''
+    fill_in 'Mínimo m³', with: ''
     fill_in 'Máximo m³', with: ''
-    fill_in 'Valor do Km', with: ''
-    click_on 'Cadastrar'
+    fill_in 'Valor por Km', with: ''
+    click_on 'Enviar'
     # Assert
     expect(page).to have_content 'Peso mínimo não pode ficar em branco'
     expect(page).to have_content 'Peso máximo não pode ficar em branco'
     expect(page).to have_content 'Mínimo m³ não pode ficar em branco'
     expect(page).to have_content 'Máximo m³ não pode ficar em branco'
-    expect(page).to have_content 'Valor do KM não pode ficar em branco'
+    expect(page).to have_content 'Valor por Km não pode ficar em branco'
   end
 
   it 'da erro se usuário não estiver logado' do
@@ -67,9 +67,8 @@ describe 'Usuário cadastra seus preços' do
     user = User.create!(email: 'admin@one.com', password: '123456', role: 'company', shipping_company: company)
     # Act
     visit shipping_companies_path
-    login_as user
     visit shipping_companies_path
     # Assert
-    expect(page).to have_content 'Você deve efetuar o login para continuar'
+    expect(page).to have_content 'Para continuar, faça login ou registre-se.'
   end
 end
